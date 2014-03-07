897d224a7a97ea071e1fca2bacd8c1db11b427b9
Thomas Levine <_@thomaslevine.com>
2014-03-07 21:28:51 +0000

    functional

--- papers/music-functions.js
+++ papers/music-functions.js
@@ -1,11 +1,9 @@
 function ionian(base, n) {
   var steps = [0, 2, 4, 5, 7, 9, 11, 12]
-  var scale = []
-  var i = 0
-  while (i < n) {
-    scale.push(base * Math.pow(2, (Math.floor(i/12) + steps[i]) * (1/12)))
-    i ++
+  function freq(i) {
+    return base * Math.pow(2, (Math.floor(i/12) + steps[i]) * (1/12))
   }
-  return scale
+  var scale = []; for (var i = 0; i < n; i++) scale.push(i)
+  return scale.map(freq)
 }
 console.log(ionian(440, 8))
