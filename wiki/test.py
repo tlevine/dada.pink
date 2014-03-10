d8c674390683a7a44ba67e5938eead34d92362b7
Thomas Levine <_@thomaslevine.com>
2014-03-10 07:14:07 +0000

    fix tests

--- wiki/test.py
+++ wiki/test.py
@@ -4,7 +4,7 @@ import os,sys
 
 import nose.tools as n
 
-exceptional_files = {'Makefile', 'index'}
+exceptional_basenames = {'Makefile', 'index', ''} # dotfiles
 exceptional_directories = {'.git','a'} # a for the Amtrak thing because I linked to it
 
 if os.path.split(os.getcwd())[-1] != 'wiki.thomaslevine.com':
@@ -21,7 +21,7 @@ def test_directory_file_correspondence():
     basenames = set(name.split('.')[0] for name in names if os.path.isfile(name))
     dirs = set(filter(os.path.isdir, names))
 
-    n.assert_set_equal(basenames.difference(exceptional_files),
+    n.assert_set_equal(basenames.difference(exceptional_basenames),
                        dirs.difference(exceptional_directories))
 
 def test_sections_are_documented():
@@ -35,7 +35,7 @@ def test_sections_are_documented():
     in_index = set()
     with open('index.rst', 'r') as fp:
         for line in fp:
-            m = re.match(r'^`/([^/]+)/ </([^/]+)/>`$', line)
+            m = re.match(r'^`/([^/]+)/ </([^/]+)/>`_$', line)
             if m:
                 n.assert_equal(m.group(1), m.group(2))
                 in_index.add(m.group(1))
