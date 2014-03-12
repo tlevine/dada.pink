b6eef9f6fca08f541c4433873ad40258e9a8e3b7
Thomas Levine <_@thomaslevine.com>
2014-03-12 11:17:18 +0000

    chek

--- wiki/test.py
+++ wiki/test.py
@@ -24,14 +24,36 @@ def test_directory_file_correspondence():
     n.assert_set_equal(basenames.difference(exceptional_basenames),
                        dirs.difference(exceptional_directories))
 
-def check_sections_are_documented(directory):
+def test_sections_are_documented():
     '''
     Each top-level directory should be referenced in the
     documentation, and each directory referenced in the
     documentation should exist.
     '''
 
-    fn = 'index.rst' if directory == '.' else directory + '.rst'
+    # In the documentation
+    in_index = set()
+    with open('index.rst', 'r') as fp:
+        for line in fp:
+            m = re.match(r'^`/([^/]+)/ </([^/]+)/>`_$', line)
+            if m:
+                n.assert_equal(m.group(1), m.group(2))
+                in_index.add(m.group(1))
+
+    # In the filesystem
+    dirs = set(filter(os.path.isdir, os.listdir()))
+
+    n.assert_set_equal(in_index, dirs.difference(exceptional_directories))
+
+def check_subsections_are_documented(directory):
+    '''
+    Each second-level file should be referenced in the
+    documentation for the appropriate top-level directory,
+    and each file referenced in the
+    documentation should exist.
+    '''
+
+    fn = directory + '.rst'
     in_index = set()
     if os.path.isfile(fn):
         # In the documentation
@@ -43,11 +65,11 @@ def check_sections_are_documented(directory):
                     in_index.add(m.group(1))
 
         # In the filesystem
-        dirs = set(filter(os.path.isdir, os.listdir(directory)))
+        dirs = set(fn.split('.')[0] for fn in os.listdir(directory))
 
         # Documentation and filesystem should match.
         n.assert_set_equal(in_index, dirs.difference(exceptional_directories))
 
-def test_sections_are_documented():
+def test_subsections_are_documented():
     for directory in filter(os.path.isdir, os.listdir()):
-        yield check_sections_are_documented, directory
+        yield check_subsections_are_documented, directory
