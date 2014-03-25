85e7fdf2acdefd897f2ca0b06919369931b95acb
Thomas Levine <_@thomaslevine.com>
2014-03-25 18:36:26 +0000

    -

--- disseminate/tu-delft/count.sh
+++ disseminate/tu-delft/count.sh
@@ -2,8 +2,8 @@
 
 printf "Bachelor's project: "
 sed '1,/Final/ d' essay.mdwn |wc -w|tr -d '\n'
-printf ' words (They request 1000 to 1500 words.)\n'
+printf ' words (They request a maximum of 250 words.)\n'
 
 printf "Full essay:         "
-cat essay.mdwn|wc -w|tr -d '\n'
-printf ' words (They request a maximum of 250 words.)\n'
+sed '1,/I want/ d' essay.mdwn |wc -w|tr -d '\n'
+printf ' words (They request 1000 to 1500 words.)\n'
