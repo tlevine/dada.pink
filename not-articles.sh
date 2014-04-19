9497b539be90ec7ef7230bb5b7d4d7628dac0ac3
Thomas Levine <_@thomaslevine.com>
2014-04-19 17:34:40 +0000

    delete

--- not-articles.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-for file in content/\!/*/index.md; do grep article "$file" > /dev/null || echo "$file"; done
--- posts-by-date.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-grep created_at content/\!/*/index.md|cut -d: -f1,3|sed 's/\([^:]*\):\([^:]*\)/\2:\1/'|sed 's/^ //'|sort
