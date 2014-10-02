907e4f5ac813dbaac64a9d733cd2915ec3cf6856
Thomas Levine <_@thomaslevine.com>
2014-10-02 08:12:37 +0000

    not college, college

--- dada/on-the-financial-value-of-college/earnings-tuition.R
+++ dada/on-the-financial-value-of-college/earnings-tuition.R
@@ -5,30 +5,39 @@ library(scales)
 # tuition <- c(europe = 5e4,
 #              us.public = 8e4,
 #              us.private = 2e5)
-tuition <- c(5e4, 2e5)
+tuition <- c(0, 2e5)
 years.invested <- 65 - 20 # the nominal working life
 stock.market.return <- 1.068 # from Greenstone & Looney, 2011
 
+lifetime.earnings <- c(
+  engineering = 2e6,
+  all.majors = 1.16e6,
+  early.childhood.education = 8e5
+)
+
 college <- data.frame(
+  tuition = rep(tuition, length(lifetime.earnings)),
+  investment = paste0('Lifetime earnings (median ',
+                      sub('\\.', ' ', names(lifetime.earnings)), ')'),
+  earnings = unname(lifetime.earnings)
+)
+
+not.college <- data.frame(
   tuition = rep(tuition, each = 3)
 )
-college$stock.market.return <- c(1.050, 1.065, 1.080)
-college$earnings <- college$tuition * college$stock.market.return ^ years.invested
-college$investment <- paste0('Stock market (',
-                             college$stock.market.return - 1,
-                             '% return)')
-college$stock.market.return <- NULL
+not.college$stock.market.return <- c(1.050, 1.065, 1.080)
+not.college$earnings <- not.college$tuition *
+                        not.college$stock.market.return ^
+                        years.invested
+not.college$investment <- paste0(
+  'Stock market (',
+  round(100 * (not.college$stock.market.return - 1), 1),
+  '% annual return)')
+not.college$stock.market.return <- NULL
 
-alternatives <- rbind(college, data.frame(
-  tuition = tuition,
-  investment = 'College',
-  earnings = 
-)
-p <- ggplot(college) +
-  aes(x = tuition, y = earnings, group = factor(stock.market.return)) +
-  scale_x_continuous('Cost of college (today dollars)',
-    limits = c(0, 2e5), labels = dollar) +
-  scale_y_continuous('Earnings (today dollars)',
-    limits = c(0, 2e6), labels = dollar) +
-  geom_line() +
-  geom_hline(yintercept = salary.earnings, linetype = 'dotted')
+alternatives <- rbind(college, not.college)
+p <- ggplot(alternatives) +
+  aes(x = tuition, y = earnings, group = investment) +
+  scale_x_continuous('Cost of college (today dollars)', labels = dollar) +
+  scale_y_continuous('Earnings (today dollars)', labels = dollar) +
+  geom_line()
