87bb6f9c503fdd5dd3e25e71b20072d748bd4ecf
Thomas Levine <_@thomaslevine.com>
2014-10-02 07:56:31 +0000

    -

--- dada/on-the-financial-value-of-college/earnings-tuition.R
+++ dada/on-the-financial-value-of-college/earnings-tuition.R
@@ -5,7 +5,7 @@ library(scales)
 # tuition <- c(europe = 5e4,
 #              us.public = 8e4,
 #              us.private = 2e5)
-tuition <- seq(5e4, 2e5, 5e4)
+tuition <- c(5e4, 2e5)
 years.invested <- 65 - 20 # the nominal working life
 stock.market.return <- 1.068 # from Greenstone & Looney, 2011
 
@@ -14,9 +14,16 @@ college <- data.frame(
 )
 college$stock.market.return <- c(1.050, 1.065, 1.080)
 college$earnings <- college$tuition * college$stock.market.return ^ years.invested
-college$stock.market.return <- college$stock.market.return - 1
-salary.earnings <- 1.16e6
+college$investment <- paste0('Stock market (',
+                             college$stock.market.return - 1,
+                             '% return)')
+college$stock.market.return <- NULL
 
+alternatives <- rbind(college, data.frame(
+  tuition = tuition,
+  investment = 'College',
+  earnings = 
+)
 p <- ggplot(college) +
   aes(x = tuition, y = earnings, group = factor(stock.market.return)) +
   scale_x_continuous('Cost of college (today dollars)',
